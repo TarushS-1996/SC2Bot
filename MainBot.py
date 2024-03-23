aaf68204333a11bee425974a213ed51d4e26effe
TarushS-1996 <tarush.singh29@gmail.com>
2024-03-22 20:39:59 -0400

    added a map randomizer which will select maps at random.

--- MainBot.py
+++ MainBot.py
@@ -2,6 +2,7 @@ import sc2
 from sc2 import Race, Difficulty, maps, run_game, position, Result
 from sc2.player import Bot, Computer
 import asyncio
+import random
 
 class MainBot(sc2.BotAI):
     async def on_step(self, iteration):
@@ -12,5 +13,18 @@ class MainBot(sc2.BotAI):
         return await super().distribute_workers(resource_ratio)
     
 
+def mapRandomizer():
+    mapSelected = [maps.get("AutomatonLE"), maps.get("PortAleksanderLE"),
+            maps.get("YearZeroLE"), maps.get("AcropolisLE"),
+            maps.get("ThunderbirdLE"), maps.get("TurboCruise'84LE"),
+            maps.get("CyberForestLE"), maps.get("KairosJunctionLE"),
+            maps.get("KingsCoveLE"), maps.get("NewRepugnancyLE")]
+    mapToSend = mapSelected[random.randint(0, len(mapSelected) - 1)]
+    print(str(mapToSend))
+    return mapToSend
+
 def main():
-    run_game(maps.get("PortAleksanderLE"), [Bot(Race.Protoss, MainBot()), Computer(Race.Protoss, Difficulty.Medium)], realtime = False)
+    run_game(mapRandomizer(), [Bot(Race.Protoss, MainBot()), Computer(Race.Protoss, Difficulty.Medium)], realtime = False)
+
+if __name__ == "__main__":
+    main()
